48606210739cf4ed7b7cbd6cf5ac7dba5f7b53cb
Yuan Zhang <zhangyuan@users.noreply.github.com>
2022-05-06 18:40:36 +0800

    parse glue job

--- pkg/workflow/glue.go
+++ pkg/workflow/glue.go
@@ -1,9 +1,105 @@
 package workflow
 
+import "github.com/pkg/errors"
+
 type GlueWorkflow struct {
 	Workflow
+	Name string
+	Jobs []GlueJob
+}
+
+type GlueJob struct {
+	Name        string
+	Description string
+	Type        string
+	Entrypoint  string
+	Args        interface{}
+	Requires    []RequiredJob
+	Tags        []Tag
+}
+
+type RequiredJob struct {
+	JobName string
+}
+
+const (
+	PythonJob  string = "python"
+	GlueSQLJob string = "glue-sql"
+	DummyJob   string = "dummy"
+)
+
+func ParseJobType(rawType string) (string, error) {
+	switch rawType {
+	case PythonJob:
+		return PythonJob, nil
+	case GlueSQLJob:
+		return GlueSQLJob, nil
+	case DummyJob:
+		return DummyJob, nil
+	}
+
+	return "", errors.Errorf("invalid job type %v", rawType)
 }
 
 func parseGlueWorkflow(rawWorkflow map[string]interface{}) (*GlueWorkflow, error) {
-	return &GlueWorkflow{}, nil
+	rawJobs := rawWorkflow["jobs"].(map[interface{}]interface{})
+
+	jobs := []GlueJob{}
+
+	for key, value := range rawJobs {
+		properties := value.(map[interface{}]interface{})
+
+		jobType, err := ParseJobType(properties["type"].(string))
+		if err != nil {
+			return nil, err
+		}
+
+		var entrypoint string
+
+		if properties["entrypoint"] == nil {
+			entrypoint = ""
+		} else {
+			entrypoint = properties["entrypoint"].(string)
+		}
+
+		requiredJobs := []RequiredJob{}
+
+		if properties["requires"] != nil {
+			rawRequires := properties["requires"].([]interface{})
+			for _, rawRequiredJob := range rawRequires {
+				rawRequiredJob := rawRequiredJob.(map[interface{}]interface{})
+
+				requiredJobs = append(requiredJobs, RequiredJob{
+					JobName: rawRequiredJob["job_name"].(string),
+				})
+			}
+		}
+
+		tags := []Tag{}
+		if properties["tags"] != nil {
+			rawTags := properties["tags"].(map[interface{}]interface{})
+			for name, value := range rawTags {
+				tags = append(tags, Tag{
+					Name:  name.(string),
+					Value: value.(string),
+				})
+			}
+		}
+
+		job := GlueJob{
+			Name:        key.(string),
+			Description: properties["description"].(string),
+			Type:        jobType,
+			Entrypoint:  entrypoint,
+			Args:        properties["args"],
+			Requires:    requiredJobs,
+			Tags:        tags,
+		}
+
+		jobs = append(jobs, job)
+	}
+	return &GlueWorkflow{
+		Name: rawWorkflow["name"].(string),
+		Jobs: jobs,
+	}, nil
 }
--- pkg/workflow/workflow_test.go
+++ pkg/workflow/workflow_test.go
@@ -13,7 +13,62 @@ func TestParseGlueWorkflow(t *testing.T) {
 
 	assert.Nil(t, err)
 
-	expected := &GlueWorkflow{}
+	expected := &GlueWorkflow{
+		Name: "my-workflow",
+		Jobs: []GlueJob{
+			{
+				Name:        "ingestion",
+				Description: "extract log from excel to s3",
+				Type:        PythonJob,
+				Entrypoint:  "raw/ingestion.py",
+				Args: map[interface{}]interface{}{
+					"source_path": "s3://sourceBucket/source/",
+					"raw_path":    "s3://rawStorageBucket/raw/",
+					"schema": map[interface{}]interface{}{
+						"id":          "int",
+						"description": "string",
+					},
+				},
+				Requires: []RequiredJob{},
+				Tags: []Tag{
+					{
+						Name: "team", Value: "fantastic-team",
+					},
+					{
+						Name: "region", Value: "us-west-1",
+					},
+				},
+			},
+			{
+				Name:        "transformation",
+				Description: "transform and load",
+				Type:        GlueSQLJob,
+				Entrypoint:  "transformations/transform.sql",
+				Args: map[interface{}]interface{}{
+					"years": []interface{}{2021, 2022},
+				},
+				Requires: []RequiredJob{
+					{
+						JobName: "ingestion",
+					},
+				},
+				Tags: []Tag{},
+			},
+			{
+				Name:        "notification",
+				Description: "dummy job",
+				Type:        DummyJob,
+				Entrypoint:  "",
+				Args:        nil,
+				Requires: []RequiredJob{
+					{
+						JobName: "transformation",
+					},
+				},
+				Tags: []Tag{},
+			},
+		},
+	}
 
 	assert.Equal(t, expected, workflow)
 }
